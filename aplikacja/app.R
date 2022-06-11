8f8a415b83e340f1b28f3591e8dbfa99d53a6fc4
anowacki <adriannowacki007@gmail.com>
2022-06-11 21:51:04 +0200

    dodano kilka opcji

--- aplikacja/app.R
+++ aplikacja/app.R
@@ -2,16 +2,45 @@
 library(shiny)
 
 # Define UI ----
+
 ui <- fluidPage(
-    titlePanel("title panel"),
+    titlePanel("Wskaznik H i Entropia census"),
     
-    sidebarLayout(position = "right",
-        sidebarPanel("sidebar panel"),
-        mainPanel("main panel")
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+            
+            selectInput("year", 
+                        label = "Choose a year",
+                        choices = c("1990", 
+                                    "2000",
+                                    "2010", 
+                                    "2020"),
+                        selected = "1990"),
+            
+            selectInput("index", 
+                        label = "Choose an index",
+                        choices = c("Entrophy",
+                                    "Index of dissimilarity", 
+                                    "The information theory index H"),
+                        selected = "Percent White"),
+            
+            selectInput("var", 
+                        label = "Choose a variable to display",
+                        choices = c("Percent White", 
+                                    "Percent Black",
+                                    "Percent Hispanic", 
+                                    "Percent Asian"),
+                        selected = "Percent White"),
+            
+        ),
+        
+        mainPanel(
+            textOutput("selected_var")
+        )
     )
 )
-
-
 # Define server logic ----
 server <- function(input, output) {
     
