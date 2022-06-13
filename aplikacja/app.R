32b08240d2d5c6b43491284ff35d85fb96f1d096
anowacki <adriannowacki007@gmail.com>
2022-06-13 17:46:07 +0200

    działaaaaa

--- aplikacja/app.R
+++ aplikacja/app.R
@@ -5,20 +5,20 @@ library(sf)
 library(tmap)
 library(shinydashboard)
 # wczytanie danych
-shp_block_1990 <- st_read("dane/counties_shp/census_shp/shp_block_1990.gpkg")
-shp_block_2000 <- st_read("dane/counties_shp/census_shp/shp_block_2000.gpkg")
-shp_block_2010 <- st_read("dane/counties_shp/census_shp/shp_block_2010.gpkg")
-shp_block_2020 <- st_read("dane/counties_shp/census_shp/shp_block_2020.gpkg")
+shp_block_1990 <- st_read("../dane/counties_shp/census_shp/shp_block_1990.gpkg")
+shp_block_2000 <- st_read("../dane/counties_shp/census_shp/shp_block_2000.gpkg")
+shp_block_2010 <- st_read("../dane/counties_shp/census_shp/shp_block_2010.gpkg")
+shp_block_2020 <- st_read("../dane/counties_shp/census_shp/shp_block_2020.gpkg")
 
-shp_grp_blocks_1990 <- st_read("dane/counties_shp/census_shp/shp_grp_blocks_1990.gpkg")
-shp_grp_blocks_2000 <- st_read("dane/counties_shp/census_shp/shp_grp_blocks_2000.gpkg")
-shp_grp_blocks_2010 <- st_read("dane/counties_shp/census_shp/shp_grp_blocks_2010.gpkg")
-shp_grp_blocks_2020 <- st_read("dane/counties_shp/census_shp/shp_grp_blocks_2020.gpkg")
+shp_grp_blocks_1990 <- st_read("../dane/counties_shp/census_shp/shp_grp_blocks_1990.gpkg")
+shp_grp_blocks_2000 <- st_read("../dane/counties_shp/census_shp/shp_grp_blocks_2000.gpkg")
+shp_grp_blocks_2010 <- st_read("../dane/counties_shp/census_shp/shp_grp_blocks_2010.gpkg")
+shp_grp_blocks_2020 <- st_read("../dane/counties_shp/census_shp/shp_grp_blocks_2020.gpkg")
 
-shp_tract_1990 <- st_read("dane/counties_shp/census_shp/shp_tract_1990.gpkg")
-shp_tract_2000 <- st_read("dane/counties_shp/census_shp/shp_tract_2000.gpkg")
-shp_tract_2010 <- st_read("dane/counties_shp/census_shp/shp_tract_2010.gpkg")
-shp_tract_2020 <- st_read("dane/counties_shp/census_shp/shp_tract_2020.gpkg")
+shp_tract_1990 <- st_read("../dane/counties_shp/census_shp/shp_tract_1990.gpkg")
+shp_tract_2000 <- st_read("../dane/counties_shp/census_shp/shp_tract_2000.gpkg")
+shp_tract_2010 <- st_read("../dane/counties_shp/census_shp/shp_tract_2010.gpkg")
+shp_tract_2020 <- st_read("../dane/counties_shp/census_shp/shp_tract_2020.gpkg")
 
 
 blocks <- list(shp_block_1990, shp_block_2000, shp_block_2010, shp_block_2020)
@@ -26,8 +26,6 @@ grp_blocks <- list(shp_grp_blocks_1990, shp_grp_blocks_2000, shp_grp_blocks_2010
 tracts <- list(shp_tract_1990, shp_tract_2000, shp_tract_2010, shp_tract_2020)
 
 obiekty <- list(blocks = blocks, grp_blocks =  grp_blocks, tracts = tracts)
-
-
 # wskazniki
 shp_ind_D_1990 <- st_read("dane/counties_shp/indexes_shp/shp_ind_D_1990.gpkg")
 shp_ind_D_2000 <- st_read("dane/counties_shp/indexes_shp/shp_ind_D_2000.gpkg")
@@ -51,69 +49,74 @@ ui <- navbarPage("Racial diversity",
                  tabPanel("Interactive map"),
                  tabPanel("Data frames"),
                  tabPanel("Author"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            helpText("Create demographic maps with 
-               information from the 2010 US Census."),
-            
-            
-            selectInput("unit", 
-                        label = "Choose an aggregation unit",
-                        choices = c("blocks", 
-                                    "grp_blocks",
-                                    "tracts"),
-                        selected = "blocks"),
-            
-            selectInput("year", 
-                        label = "Choose a year",
-                        choices = c("1990", 
-                                    "2000",
-                                    "2010", 
-                                    "2020"),
-                        selected = "1990"),
-            
-            selectInput("index", 
-                        label = "Choose an index",
-                        choices = c("Entrophy",
-                                    "Index of dissimilarity", 
-                                    "The information theory index H"),
-                        selected = "Entrophy"),
-            actionButton("action", "Show")
-            
-        ),
-        
-        mainPanel(
-            tabsetPanel(
-                tabPanel("Map", tmapOutput("map")), 
-                tabPanel("Plot", verbatimTextOutput("plot")), 
-                tabPanel("Table", tableOutput("table"))
-            )
-        )
-    )
+                 
+                 sidebarLayout(
+                     sidebarPanel(
+                         helpText("All indicators for individual years"),
+                         
+                         
+                         selectInput("unit", 
+                                     label = "Choose an aggregation unit",
+                                     choices = c("blocks", 
+                                                 "grp_blocks",
+                                                 "tracts"),
+                                     selected = "blocks"),
+                         
+                         selectInput("year", 
+                                     label = "Choose a year",
+                                     choices = c("1990", 
+                                                 "2000",
+                                                 "2010", 
+                                                 "2020"),
+                                     selected = "1990"),
+                         
+                         helpText("Selected indicator for individual aggregation units"),
+                         
+                         selectInput("index", 
+                                     label = "Choose an index",
+                                     choices = c("Entrophy",
+                                                 "Index of dissimilarity", 
+                                                 "The information theory index H"),
+                                     selected = "Entrophy"),
+                         actionButton("run", "Dawaj kurwa")
+                     ),
+                     
+                     mainPanel(
+                         tabsetPanel(
+                             tabPanel("Map", tmapOutput("map")), 
+                             tabPanel("Plot", verbatimTextOutput("plot")), 
+                             tabPanel("Table", tableOutput("table"))
+                         )
+                     )
+                 )
 )
 
 # Define server logic ----
 server <- function(input, output) {
+    warstwa <- eventReactive(input$run, {
+        
+        plik <- switch(input$unit, 
+                       "blocks" = obiekty[[1]],
+                       "grp_blocks" = obiekty[[2]],
+                       "tracts" = obiekty[[3]])
+        
+        switch(input$year, 
+               "1990" = plik[[1]],
+               "2000" = plik[[2]],
+               "2010" = plik[[3]],
+               "2020" = plik[[4]])
+    })
     tmap_mode("view")
     output$map <- renderTmap({
-        plik <- switch(input$unit, 
-                       "blocks" = obiekty$blocks,
-                       "grp_blocks" = obiekty$grp_blocks,
-                       "tracts" = obiekty$tracts)
+        warstwa <- warstwa()
+        tm_shape(warstwa) + tm_fill(col = "Entropia_std", 
+                                    id = "NAMELSAD",
+                                    popup.vars = c("Entropia: " = "Entropia", "Entropia std: " = "Entropia_std", 
+                                                   "H: " = "H", "D (white-black)" = "D_wb", "D (white-asian)" = "D_wa", 
+                                                   "D (white-latin)" = "D_wl", "D (black-latin)" = "D_bl", 
+                                                   "D (black-asian)" = "D_ba", "D (latin-asian)" = "D_la")) + tm_borders()
         
-        rok <- switch(input$year, 
-                       "1990" = plik[1],
-                       "2000" = plik[2],
-                       "2010" = plik[3],
-                       "2020" = plik[4])
         
-        tm_shape(rok) + tm_fill(col = "Entropia_std", 
-                                           id = "NAMELSAD",
-                                           popup.vars = c("Entropia: " = "Entropia", "Entropia std: " = "Entropia_std", 
-                                                          "H: " = "H", "D (white-black)" = "D_wb", "D (white-asian)" = "D_wa", 
-                                                          "D (white-latin)" = "D_wl", "D (black-latin)" = "D_bl", 
-                                                          "D (black-asian)" = "D_ba", "D (latin-asian)" = "D_la")) + tm_borders()
     })
 }
 
